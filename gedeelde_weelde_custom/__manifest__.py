af9aaedbefb19e38756fafda662279c123ccf493
Jur de Vries <info@jurdevries.nl>
2025-07-17 12:50:54 +0200

    feat: GED-119: added discount to customer facing display

--- gedeelde_weelde_custom/__manifest__.py
+++ gedeelde_weelde_custom/__manifest__.py
@@ -26,6 +26,7 @@
             "gedeelde_weelde_custom/static/src/js/extend_products_screen_barcode.js",
             "gedeelde_weelde_custom/static/src/js/pos_auto_order.js",
             "gedeelde_weelde_custom/static/src/xml/receipt_screen.xml",
+            "gedeelde_weelde_custom/static/src/xml/customer_facing_display_order_line_template.xml",
             "gedeelde_weelde_custom/static/src/css/pos_custom.scss",
         ],
     },
