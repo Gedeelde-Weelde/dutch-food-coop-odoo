6573c36cc4ca5569f7b61e634b0c9c6e891b525f
Jur de Vries <info@jurdevries.nl>
2025-08-04 10:27:08 +0200

    feat: GED-121: disable invoice when pay_later is removed

--- gedeelde_weelde_custom/static/src/js/extend_payment_screen.js
+++ gedeelde_weelde_custom/static/src/js/extend_payment_screen.js
@@ -1,25 +1,48 @@
-odoo.define('gedeelde_weelde_custom.PaymentScreen', function (require) {
-    'use strict';
+odoo.define("gedeelde_weelde_custom.PaymentScreen", function (require) {
+    "use strict";
 
-    const PaymentScreen = require('point_of_sale.PaymentScreen');
-    const Registries = require('point_of_sale.Registries');
+    const PaymentScreen = require("point_of_sale.PaymentScreen");
+    const Registries = require("point_of_sale.Registries");
 
     const CustomPaymentScreen = (PaymentScreen) =>
         class extends PaymentScreen {
-            addNewPaymentLine({ detail: paymentMethod }) {
-                const result = super.addNewPaymentLine({ detail: paymentMethod });
+            addNewPaymentLine({detail: paymentMethod}) {
+                const result = super.addNewPaymentLine({detail: paymentMethod});
 
-                this.handleInvoiceToggle(result, paymentMethod);
+                if (result) {
+                    console.log("Adding new payment line", paymentMethod.cid);
+                    this.enableInvoice(paymentMethod);
+                }
 
                 return result;
             }
 
-            handleInvoiceToggle(result, paymentMethod) {
-                if (result && paymentMethod.type === 'pay_later') {
+            enableInvoice(paymentMethod) {
+                if (paymentMethod.type === "pay_later") {
                     if (!this.currentOrder.is_to_invoice()) {
                         this.toggleIsToInvoice();
                     }
                 }
+            }
+
+            disableInvoice(paymentMethod) {
+                if (paymentMethod.type === "pay_later") {
+                    if (this.currentOrder.is_to_invoice()) {
+                        this.toggleIsToInvoice();
+                    }
+                }
+            }
+
+            deletePaymentLine(event) {
+                const line = this.paymentLines.find(
+                    (line) => line.cid === event.detail.cid
+                );
+                const paymentMethod = line.payment_method;
+                if (paymentMethod) {
+                    this.disableInvoice(paymentMethod);
+                }
+
+                super.deletePaymentLine(event);
             }
         };
 
