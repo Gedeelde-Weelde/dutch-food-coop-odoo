7b21c04ce2d2ac22547eb7cd0abc7378e937490c
Jur de Vries <info@jurdevries.nl>
2025-09-11 13:24:29 +0200

    bug: GED-123: added more logging

--- gw_pos_logging/__manifest__.py
+++ gw_pos_logging/__manifest__.py
@@ -1,10 +1,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "POS Payment Logging",
+    "name": "POS Order Logging",
     "version": "16.0.1.0.0",
     "category": "Point of Sale",
-    "summary": "Logs POS payment operations to browser database",
+    "summary": "Logs POS order operations and payment events to browser database",
     "author": "Gedeelde Weelde",
     "website": "https://github.com/Gedeelde-Weelde/dutch-food-coop-odoo",
     "license": "AGPL-3",
@@ -13,6 +13,7 @@
     "assets": {
         "point_of_sale.assets": [
             "gw_pos_logging/static/src/js/pos_payment_logging.js",
+            "gw_pos_logging/static/src/js/pos_order_logging.js",
         ],
     },
     "installable": True,
