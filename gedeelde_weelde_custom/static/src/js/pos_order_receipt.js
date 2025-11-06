4bad8af28a197269797b39303f3fb80b26c02307
Jur de Vries <info@jurdevries.nl>
2025-11-06 12:50:13 +0100

    feat: GM-163: Added total discount

--- gedeelde_weelde_custom/static/src/js/pos_order_receipt.js
+++ gedeelde_weelde_custom/static/src/js/pos_order_receipt.js
@@ -11,14 +11,20 @@ odoo.define("gedeelde_weelde_custom.Order", function (require) {
         class extends Order {
             get_total_without_discount_lines() {
                 const normalOrderLines = this.orderlines.filter(orderline => orderline.product.default_code !== 'DISC')
-                let result = round_pr(normalOrderLines.reduce((function(sum, orderLine) {
+                return round_pr(normalOrderLines.reduce((function (sum, orderLine) {
                     return sum + orderLine.get_price_with_tax();
                 }), 0), this.pos.currency.rounding);
-                return result;
+            }
+            get_total_of_discount_lines() {
+                const discountOrderLines = this.orderlines.filter(orderline => orderline.product.default_code === 'DISC')
+                return round_pr(discountOrderLines.reduce((function (sum, orderLine) {
+                    return sum + orderLine.get_price_with_tax();
+                }), 0), this.pos.currency.rounding);
             }
             export_for_printing() {
                 let receipt = super.export_for_printing();
                 receipt.total_without_discount_lines = this.get_total_without_discount_lines();
+                receipt.total_of_discount_lines = this.get_total_of_discount_lines();
                 return receipt;
             }
         };
