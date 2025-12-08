12b3ae4f4a133cab80a08f22e5480401747cca8e
Jur de Vries <info@jurdevries.nl>
2025-12-08 14:15:33 +0100

    bug: GED-134: implemented new way of calculating cost prices

--- gw_margin_repair/models/pos_order_line.py
+++ gw_margin_repair/models/pos_order_line.py
@@ -1,8 +1,29 @@
-from odoo import models
+from odoo import models, fields
 
 
 class PosOrderLine(models.Model):
     _inherit = "pos.order.line"
 
     def _compute_total_cost(self, stock_moves):
-        return super()._compute_total_cost(stock_moves)
+        """
+        Compute the total cost of the order lines.
+        :param stock_moves: recordset of `stock.move`, used for fifo/avco lines
+        """
+        for line in self.filtered(lambda l: not l.is_total_cost_computed):
+            product = line.product_id
+            if line._is_product_storable_fifo_avco() and stock_moves:
+                product_cost = product._compute_average_price(0, line.qty,
+                                                              line._get_stock_moves_to_consider(stock_moves, product))
+            elif line.price_unit != product.list_price:
+                product_cost = (line.price_unit / product.list_price) * product.standard_price
+            else:
+                product_cost = product.standard_price
+            line.total_cost = line.qty * product.cost_currency_id._convert(
+                from_amount=product_cost,
+                to_currency=line.currency_id,
+                company=line.company_id or self.env.company,
+                date=line.order_id.date_order or fields.Date.today(),
+                round=False,
+            )
+            line.is_total_cost_computed = True
+
--- gw_margin_repair/tests/test_pos_order_line.py
+++ gw_margin_repair/tests/test_pos_order_line.py
@@ -3,10 +3,14 @@ from pickle import FALSE
 from odoo import fields
 from odoo.tests import TransactionCase
 
+list_order_price = 15.0
+standard_price = 38.5
+list_price = 58.9
 
 class TestPosOrderLineTotalCost(TransactionCase):
     def setUp(self):
         super().setUp()
+
 
         # Create currency
         self.currency = self.env["res.currency"].create(
@@ -21,8 +25,8 @@ class TestPosOrderLineTotalCost(TransactionCase):
         self.product = self.env["product.product"].create(
             {
                 "name": "Test Product",
-                "standard_price": 10.0,
-                "list_price": 20.0,
+                "standard_price": standard_price,
+                "list_price": list_price,
                 "cost_currency_id": self.currency.id,
             }
         )
@@ -48,12 +52,29 @@ class TestPosOrderLineTotalCost(TransactionCase):
                 "date_order": fields.Datetime.now(),
                 "company_id": self.env.company.id,
                 "amount_tax": 0.0,
-                "amount_total": 30.0,
+                "amount_total": list_order_price,
                 "amount_paid": 0.0,
                 "amount_return": 0.0,
             }
         )
 
+    def test_compute_total_cost_standard_product(self):
+        # Create POS order line
+        self.order_line = self.env["pos.order.line"].create(
+            {
+                "order_id": self.pos_order.id,
+                "product_id": self.product.id,
+                "qty": 1.0,
+                "name": "Test Line",
+                "price_subtotal": 13.76,
+                "price_subtotal_incl": list_order_price,
+                "price_unit": list_order_price
+            }
+        )
+        self.order_line._compute_total_cost(FALSE)
+        self.assertEqual(self.order_line.total_cost, 9.8)
+
+    def test_compute_total_cost_standard_product_mulitple(self):
         # Create POS order line
         self.order_line = self.env["pos.order.line"].create(
             {
@@ -61,11 +82,10 @@ class TestPosOrderLineTotalCost(TransactionCase):
                 "product_id": self.product.id,
                 "qty": 2.0,
                 "name": "Test Line",
-                "price_subtotal": 30.0,
-                "price_subtotal_incl": 30.0,
+                "price_subtotal": 27.52,
+                "price_subtotal_incl": list_order_price * 2,
+                "price_unit": list_order_price
             }
         )
-
-    def test_compute_total_cost_standard_product(self):
-        total_cost = self.order_line._compute_total_cost(FALSE)
-        self.assertEqual(total_cost, 60.0)
+        self.order_line._compute_total_cost(FALSE)
+        self.assertEqual(self.order_line.total_cost, 19.61)
