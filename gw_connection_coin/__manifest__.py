24d82bd5328b1ce400ac03926a53ad454bd65e1e
Jur de Vries <info@jurdevries.nl>
2026-04-02 17:53:35 +0200

    feat: GM-247: Created new module for connection coin.

---
+++ gw_connection_coin/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Gedeelde Weelde Connection Coin',
+    'version': '16.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Connection Coin for Gedeelde Weelde',
+    'description': """
+        Gedeelde Weelde Connection Coin module.
+    """,
+    'author': 'Gedeelde Weelde',
+    "website": "https://github.com/Gedeelde-Weelde/dutch-food-coop-odoo",
+    'license': 'AGPL-3',
+    'depends': ['point_of_sale'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
