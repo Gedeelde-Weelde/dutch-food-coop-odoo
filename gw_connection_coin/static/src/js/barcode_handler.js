07a710ba5596812b35de507cbde3ef0364b91981
Jur de Vries <info@jurdevries.nl>
2026-04-14 14:12:38 +0200

    feat: GM-217: Added applying discount when user barcode is read.

--- gw_connection_coin/static/src/js/barcode_handler.js
+++ gw_connection_coin/static/src/js/barcode_handler.js
@@ -1,6 +1,7 @@
 odoo.define('gw_connection_coin.ProductScreen', function (require) {
     'use strict';
 
+    const DiscountButton = require('pos_discount.DiscountButton');
     const ProductScreen = require('point_of_sale.ProductScreen');
     const Registries = require('point_of_sale.Registries');
 
@@ -9,11 +10,15 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
             _barcodePartnerAction(code) {
                 const partner = this.env.pos.db.get_partner_by_barcode(code.code);
                 if (partner) {
+                    console.log('Scanned contact met discount:', partner);
                     this.showPopup('ConfirmPopup', {
                         title: this.env._t('Contact Scanned'),
                         body: _.str.sprintf(this.env._t('Scanned contact: %s'), partner.name),
                     });
                 }
+                // Call apply_discount with the ProductScreen as context
+                    // since it shares the same this.env and this.showPopup
+                DiscountButton.prototype.apply_discount.call(this, this.env.pos.config.discount_pc);
                 return super._barcodePartnerAction(code);
             }
         };
