31ded320e57e9e9042eb5d684f7856752f6a0fc5
Jur de Vries <info@jurdevries.nl>
2026-05-18 16:53:32 +0200

    GM-217: fixed formatting

--- gw_connection_coin/__manifest__.py
+++ gw_connection_coin/__manifest__.py
@@ -1,21 +1,21 @@
 {
-    'name': 'Gedeelde Weelde Connection Coin',
-    'version': '16.0.1.0.0',
-    'category': 'Point of Sale',
-    'summary': 'Connection Coin for Gedeelde Weelde',
-    'description': """
+    "name": "Gedeelde Weelde Connection Coin",
+    "version": "16.0.1.0.0",
+    "category": "Point of Sale",
+    "summary": "Connection Coin for Gedeelde Weelde",
+    "description": """
         Gedeelde Weelde Connection Coin module.
     """,
-    'author': 'Gedeelde Weelde',
+    "author": "Gedeelde Weelde",
     "website": "https://github.com/Gedeelde-Weelde/dutch-food-coop-odoo",
-    'license': 'AGPL-3',
-    'depends': ['point_of_sale'],
-    'assets': {
-        'point_of_sale.assets': [
-            'gw_connection_coin/static/src/js/barcode_handler.js',
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale.assets": [
+            "gw_connection_coin/static/src/js/barcode_handler.js",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
--- gw_connection_coin/static/src/js/barcode_handler.js
+++ gw_connection_coin/static/src/js/barcode_handler.js
@@ -1,9 +1,9 @@
-odoo.define('gw_connection_coin.ProductScreen', function (require) {
-    'use strict';
+odoo.define("gw_connection_coin.ProductScreen", function (require) {
+    "use strict";
 
-    const DiscountButton = require('pos_discount.DiscountButton');
-    const ProductScreen = require('point_of_sale.ProductScreen');
-    const Registries = require('point_of_sale.Registries');
+    const DiscountButton = require("pos_discount.DiscountButton");
+    const ProductScreen = require("point_of_sale.ProductScreen");
+    const Registries = require("point_of_sale.Registries");
 
     const GWConnectionCoinProductScreen = (ProductScreen) =>
         class extends ProductScreen {
@@ -17,7 +17,11 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
             }
             _setValue(val) {
                 super._setValue(val);
-                if (this.env.pos.numpadMode === 'quantity' || this.env.pos.numpadMode === 'discount' || this.env.pos.numpadMode === 'price') {
+                if (
+                    this.env.pos.numpadMode === "quantity" ||
+                    this.env.pos.numpadMode === "discount" ||
+                    this.env.pos.numpadMode === "price"
+                ) {
                     this._updateDiscount();
                 }
             }
@@ -26,7 +30,10 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
                 this._updateDiscount();
             }
             _getDiscountProductId() {
-                return this.env.pos.config.discount_product_id && this.env.pos.config.discount_product_id[0];
+                return (
+                    this.env.pos.config.discount_product_id &&
+                    this.env.pos.config.discount_product_id[0]
+                );
             }
             _applyDiscount() {
                 const discountProductId = this._getDiscountProductId();
@@ -36,7 +43,10 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
                 const order = this.env.pos.get_order();
                 const selectedLine = order.get_selected_orderline();
                 const currentMode = this.env.pos.numpadMode;
-                DiscountButton.prototype.apply_discount.call(this, this.env.pos.config.discount_pc);
+                DiscountButton.prototype.apply_discount.call(
+                    this,
+                    this.env.pos.config.discount_pc
+                );
                 if (selectedLine && selectedLine.product.id !== discountProductId) {
                     order.select_orderline(selectedLine);
                     if (this.env.pos.numpadMode !== currentMode) {
@@ -50,7 +60,9 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
                     return;
                 }
                 const order = this.env.pos.get_order();
-                const discountLine = order.get_orderlines().find(line => line.product.id === discountProductId);
+                const discountLine = order
+                    .get_orderlines()
+                    .find((line) => line.product.id === discountProductId);
                 if (discountLine) {
                     this._applyDiscount();
                 }
@@ -65,18 +77,35 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
                 expiryDate.setHours(0, 0, 0, 0);
 
                 if (expiryDate < today) {
-                    this.showPopup('ErrorPopup', {
-                        title: this.env._t('Connection Coin is verlopen'),
-                        body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) expired on %s.'), partner.name, partner.x_cc_nummer, partner.x_cc_einde),
+                    this.showPopup("ErrorPopup", {
+                        title: this.env._t("Connection Coin is verlopen"),
+                        body: _.str.sprintf(
+                            this.env._t(
+                                "The Connection Coin for %s (Number: %s) expired on %s."
+                            ),
+                            partner.name,
+                            partner.x_cc_nummer,
+                            partner.x_cc_einde
+                        ),
                     });
                     return false;
                 }
 
-                const diffDays = Math.ceil((expiryDate - today) / (1000 * 60 * 60 * 24));
+                const diffDays = Math.ceil(
+                    (expiryDate - today) / (1000 * 60 * 60 * 24)
+                );
                 if (diffDays >= 0 && diffDays <= 7) {
-                    this.showPopup('ConfirmPopup', {
-                        title: this.env._t('Connection Coin verlopen'),
-                        body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) will expire in %s days, on %s.'), partner.name, partner.x_cc_nummer, diffDays, partner.x_cc_einde),
+                    this.showPopup("ConfirmPopup", {
+                        title: this.env._t("Connection Coin verlopen"),
+                        body: _.str.sprintf(
+                            this.env._t(
+                                "The Connection Coin for %s (Number: %s) will expire in %s days, on %s."
+                            ),
+                            partner.name,
+                            partner.x_cc_nummer,
+                            diffDays,
+                            partner.x_cc_einde
+                        ),
                     });
                 }
                 return true;
