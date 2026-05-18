3ca6e0e03061906e91041c05ccce0ec30778ca1b
Jur de Vries <info@jurdevries.nl>
2026-05-18 16:47:42 +0200

    GM-217: refactored barcode handler in order to remove code duplication

--- gw_connection_coin/static/src/js/barcode_handler.js
+++ gw_connection_coin/static/src/js/barcode_handler.js
@@ -25,62 +25,73 @@ odoo.define('gw_connection_coin.ProductScreen', function (require) {
                 super._barcodeDiscountAction(code);
                 this._updateDiscount();
             }
-            _updateDiscount() {
-                const order = this.env.pos.get_order();
-                const discountProductId = this.env.pos.config.discount_product_id && this.env.pos.config.discount_product_id[0];
+            _getDiscountProductId() {
+                return this.env.pos.config.discount_product_id && this.env.pos.config.discount_product_id[0];
+            }
+            _applyDiscount() {
+                const discountProductId = this._getDiscountProductId();
                 if (!discountProductId) {
                     return;
                 }
-                const discountLine = order.get_orderlines().find(line => line.product.id === discountProductId);
-                if (discountLine) {
-                    const selectedLine = order.get_selected_orderline();
-                    const currentMode = this.env.pos.numpadMode;
-                    DiscountButton.prototype.apply_discount.call(this, this.env.pos.config.discount_pc);
-                    if (selectedLine && selectedLine.product.id !== discountProductId) {
-                        order.select_orderline(selectedLine);
-                        if (this.env.pos.numpadMode !== currentMode) {
-                            this.env.pos.numpadMode = currentMode;
-                        }
+                const order = this.env.pos.get_order();
+                const selectedLine = order.get_selected_orderline();
+                const currentMode = this.env.pos.numpadMode;
+                DiscountButton.prototype.apply_discount.call(this, this.env.pos.config.discount_pc);
+                if (selectedLine && selectedLine.product.id !== discountProductId) {
+                    order.select_orderline(selectedLine);
+                    if (this.env.pos.numpadMode !== currentMode) {
+                        this.env.pos.numpadMode = currentMode;
                     }
                 }
             }
+            _updateDiscount() {
+                const discountProductId = this._getDiscountProductId();
+                if (!discountProductId) {
+                    return;
+                }
+                const order = this.env.pos.get_order();
+                const discountLine = order.get_orderlines().find(line => line.product.id === discountProductId);
+                if (discountLine) {
+                    this._applyDiscount();
+                }
+            }
+            _checkConnectionCoinExpiry(partner) {
+                if (!partner.x_cc_einde) {
+                    return true;
+                }
+                const today = new Date();
+                today.setHours(0, 0, 0, 0);
+                const expiryDate = new Date(partner.x_cc_einde);
+                expiryDate.setHours(0, 0, 0, 0);
+
+                if (expiryDate < today) {
+                    this.showPopup('ErrorPopup', {
+                        title: this.env._t('Connection Coin is verlopen'),
+                        body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) expired on %s.'), partner.name, partner.x_cc_nummer, partner.x_cc_einde),
+                    });
+                    return false;
+                }
+
+                const diffDays = Math.ceil((expiryDate - today) / (1000 * 60 * 60 * 24));
+                if (diffDays >= 0 && diffDays <= 7) {
+                    this.showPopup('ConfirmPopup', {
+                        title: this.env._t('Connection Coin verlopen'),
+                        body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) will expire in %s days, on %s.'), partner.name, partner.x_cc_nummer, diffDays, partner.x_cc_einde),
+                    });
+                }
+                return true;
+            }
             _barcodePartnerAction(code) {
                 const partner = this.env.pos.db.get_partner_by_barcode(code.code);
-                if (partner) {
-                    const today = new Date();
-                    today.setHours(0, 0, 0, 0);
-                    if (partner.x_cc_einde) {
-                        const expiryDate = new Date(partner.x_cc_einde);
-                        expiryDate.setHours(0, 0, 0, 0);
-                        if (expiryDate < today) {
-                            this.showPopup('ErrorPopup', {
-                                title: this.env._t('Connection Coin is verlopen'),
-                                body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) expired on %s.'), partner.name, partner.x_cc_nummer, partner.x_cc_einde),
-                            });
-                            return;
-                        }
-                        const diffTime = expiryDate - today;
-                        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                        if (diffDays >= 0 && diffDays <= 7) {
-                            this.showPopup('ConfirmPopup', {
-                                title: this.env._t('Connection Coin verlopen'),
-                                body: _.str.sprintf(this.env._t('The Connection Coin for %s (Number: %s) will expire in %s days, on %s.'), partner.name, partner.x_cc_nummer, diffDays, partner.x_cc_einde),
-                            });
-                        }
-                    }
+                if (!partner) {
+                    return super._barcodePartnerAction(code);
+                }
 
+                const isConnectionCoinValid = this._checkConnectionCoinExpiry(partner);
+                if (isConnectionCoinValid) {
                     const order = this.env.pos.get_order();
                     order.set_partner(partner);
-                    const discountProductId = this.env.pos.config.discount_product_id && this.env.pos.config.discount_product_id[0];
-                    const selectedLine = order.get_selected_orderline();
-                    const currentMode = this.env.pos.numpadMode;
-                    DiscountButton.prototype.apply_discount.call(this, this.env.pos.config.discount_pc);
-                    if (selectedLine && selectedLine.product.id !== discountProductId) {
-                        order.select_orderline(selectedLine);
-                        if (this.env.pos.numpadMode !== currentMode) {
-                            this.env.pos.numpadMode = currentMode;
-                        }
-                    }
+                    this._applyDiscount();
                 }
                 return super._barcodePartnerAction(code);
             }
