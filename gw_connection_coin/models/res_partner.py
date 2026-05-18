a4fcd89175dc761b2cda5ce0bce57b68d9af9152
Jur de Vries <info@jurdevries.nl>
2026-05-18 17:52:09 +0200

    GM-217: added new view for simple contact add for connection coin

--- gw_connection_coin/models/res_partner.py
+++ gw_connection_coin/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -7,3 +7,27 @@ class ResPartner(models.Model):
     x_cc_nummer = fields.Char(string="CC Nummer")
     x_cc_verleng = fields.Date(string="CC Verlengdatum")
     x_cc_einde = fields.Date(string="CC Einddatum")
+
+    same_name_partner_id = fields.Many2one(
+        "res.partner", string="Partner with same name", compute="_compute_duplicates"
+    )
+    potential_duplicate_ids = fields.Many2many(
+        "res.partner",
+        compute="_compute_duplicates",
+        string="Potential Duplicates",
+    )
+
+    @api.depends("name", "firstname", "lastname")
+    def _compute_duplicates(self):
+        for partner in self:
+            if not partner.name:
+                partner.same_name_partner_id = False
+                partner.potential_duplicate_ids = False
+                continue
+
+            # Search for partners with the same name, excluding current one
+            domain = [("name", "ilike", partner.name), ("id", "!=", partner._origin.id)]
+            duplicates = self.search(domain)
+
+            partner.potential_duplicate_ids = duplicates
+            partner.same_name_partner_id = duplicates[0] if duplicates else False
