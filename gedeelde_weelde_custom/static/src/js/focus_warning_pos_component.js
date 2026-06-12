4dbc2ba598ebf42b4733a84900690b80cb781a55
Jur de Vries <info@jurdevries.nl>
2026-06-12 08:20:07 +0200

    GM-219: less aggressive focus warning

--- gedeelde_weelde_custom/static/src/js/focus_warning_pos_component.js
+++ gedeelde_weelde_custom/static/src/js/focus_warning_pos_component.js
@@ -12,23 +12,50 @@ odoo.define("gedeelde_weelde_custom.FocusWarning", function (require) {
 
                 this.focusWarningState = useState({show: false});
 
+                this._mouseInside = true;
+                this._windowFocused = document.hasFocus();
+
+                this._updateWarning = () => {
+                    const shouldWarn = !this._mouseInside && !this._windowFocused;
+                    if (shouldWarn && !this.focusWarningState.show) {
+                        this.focusWarningState.show = true;
+                    } else if (!shouldWarn) {
+                        this.focusWarningState.show = false;
+                    }
+                };
+
                 this._onMouseLeave = () => {
-                    this.focusWarningState.show = true;
-                    this.playSound("error");
+                    this._mouseInside = false;
+                    this._updateWarning();
                 };
 
                 this._onMouseEnter = () => {
-                    this.focusWarningState.show = false;
+                    this._mouseInside = true;
+                    this._updateWarning();
+                };
+
+                this._onWindowBlur = () => {
+                    this._windowFocused = false;
+                    this._updateWarning();
+                };
+
+                this._onWindowFocus = () => {
+                    this._windowFocused = true;
+                    this._updateWarning();
                 };
 
                 onMounted(() => {
                     document.body.addEventListener("mouseleave", this._onMouseLeave);
                     document.body.addEventListener("mouseenter", this._onMouseEnter);
+                    window.addEventListener("blur", this._onWindowBlur);
+                    window.addEventListener("focus", this._onWindowFocus);
                 });
 
                 onWillUnmount(() => {
                     document.body.removeEventListener("mouseleave", this._onMouseLeave);
                     document.body.removeEventListener("mouseenter", this._onMouseEnter);
+                    window.removeEventListener("blur", this._onWindowBlur);
+                    window.removeEventListener("focus", this._onWindowFocus);
                 });
             }
         };
